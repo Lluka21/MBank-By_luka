aa6e6b34ad8595c63ccc2e982300f960acd48605
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-08-17 12:23:28 +0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-// https://docs.expo.dev/guides/using-eslint
+// https://docs.expo.dev/guides/using-esli
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
