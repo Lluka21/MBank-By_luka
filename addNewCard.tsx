9f57e677bebd6ff9be2eadbf71fd10e9dfa406e8
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-08-19 12:52:36 +0400

    Update addNewCard.tsx

--- addNewCard.tsx
+++ addNewCard.tsx
@@ -12,8 +12,7 @@
 //         />
 //         <Text style={styles.label}>Color</Text>
 //         <View style={styles.colorRow}>
-//        
-//         </View>
+//            
 //         <Text style={styles.label}>Name</Text>
 //         <TextInput
 //           placeholder=""
@@ -201,6 +200,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
 });
+
 
 
 
