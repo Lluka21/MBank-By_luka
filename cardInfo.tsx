4171bae8a51f6cc3ea35f07702e98746ec1e4a17
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-08-28 09:59:22 +0400

    Update cardInfo.tsx

--- cardInfo.tsx
+++ cardInfo.tsx
@@ -11,7 +11,7 @@ import { BankCard } from "./card";
 
 export default function cardInfo() {
  
-  const active:number = 2
+  const active:number 
    
   return (
     <ScrollView
@@ -249,6 +249,7 @@ const styles = StyleSheet.create({
     width: 340,
   },
 });
+
 
 
 
