d8ecd84043efafa43f60458df22bcfc1e484cf8d
Luka Kartvelishvili <155449325+Lluka21@users.noreply.github.com>
2025-09-11 22:11:24 +0400

    Update BalanceContext.tsx

--- BalanceContext.tsx
+++ BalanceContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, ReactNode, useState } from "react";
 
 type CalculateBalanceContextType = {
-  currentBalance: numb
+  currentBalance: num
   setCurrentBalance: React.Dispatch<React.SetStateAction<number>>;
   formattedcurrBal: string;
   userInput: string;
@@ -48,6 +48,7 @@ export default function CalculateBalanceProvider({
     </CalculateBalanceContext.Provider>
   );
 }
+
 
 
 
